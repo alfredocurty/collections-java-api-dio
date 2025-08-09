4d582e4d6110e27f753e5ad28a5193450057c5fd
Alfredo Curty <curtylima@outlook.com>
2025-08-09 04:32:57 -0300

    Resolvendo exercicios Produto e CadastroProdutos
    Signed-off-by: Alfredo Curty <curtylima@outlook.com>

---
+++ src/Convidado.java
@@ -0,0 +1,42 @@
+import java.util.Objects;
+
+public class Convidado {
+    private String nome;
+    private int codigoConvite;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getCodigoConvite() {
+        return codigoConvite;
+    }
+
+    public Convidado(String nome, int codigoConvite) {
+        this.nome = nome;
+        this.codigoConvite = codigoConvite;
+
+
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Convidado convidado = (Convidado) o;
+        return codigoConvite == convidado.codigoConvite;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(codigoConvite);
+    }
+
+    @Override
+    public String toString() {
+        return "Convidado{" +
+                "nome='" + nome + '\'' +
+                ", codigoConvite=" + codigoConvite +
+                '}';
+    }
+
+}
